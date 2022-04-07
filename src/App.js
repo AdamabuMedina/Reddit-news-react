90a3be2585373a82f373e53dbf4d43d153ce51e6
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-04-07 18:42:14 +0300

    изменение версий библиотек

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function App() {
-  return (
-    <div className="app">
-      <h1>Header!!!123</h1>
-    </div>
-  );
-}
--- src/server/indexTemplate.js
+++ src/server/indexTemplate.js
@@ -1,4 +1,4 @@
-export const indexTemplate = (content) => `
+export const indexTemplate = (content)=>`
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,12 +6,12 @@ export const indexTemplate = (content) => `
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Reddit-news-react</title>
+  <title>Reddit</title>
   <script src="/static/client.js" type="application/javascript"></script>
 </head>
 
 <body>
-  <div id="root">${content}</div>
+  <div id="react_root">${content}</div>
 </body>
 
 </html>
