6164ebc1f73ca2eba9225902bef4ad956b86249f
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-04-07 17:33:21 +0300

    add ssr

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
+import React from 'react';
+
+export function App() {
   return (
     <div className="app">
-
+      <h1>Header!!!123</h1>
     </div>
   );
 }
-
-export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,8 @@
-const path = require("path")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const clientConfig = require('./cfg/webpack.client.config');
+const serverConfig = require('./cfg/webpack.server.config');
 
-const NODE_ENV = process.env.NODE_ENV
-const IS_DEV = NODE_ENV === "development"
 
-module.exports = {
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"]
-  },
-  mode: NODE_ENV ? NODE_ENV : "development",
-  entry: path.resolve(__dirname, "src/index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js"
-  },
-  devServer: {
-    open: true,
-    hot: IS_DEV,
-    port: 3000,
-  },
-  module: {
-    rules: [{
-      test: /\.[tj]s[x]?$/,
-      use: ["ts-loader"]
-    }]
-  },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Reddit-news-react',
-    template: path.resolve(__dirname, "./public/index.html")
-  })]
-}
+module.exports = [
+  clientConfig,
+  serverConfig,
+];
